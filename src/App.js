4afb2c6d1439f614d6b3d764bf86e49aee8308dc
Hamza <108789747+hamzajuwaihan@users.noreply.github.com>
2022-11-15 16:05:43 +0300

    add sections to landing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Address } from './components/landing/Address';
+import { NavBar } from './components/landing/NavBar';
+import Welcome from './components/landing/Welcome';
 
 function App() {
   return (
-    <div>
+    <>
+      <Address />
+      <NavBar />
+      <Routes>
+        <Route path="/" element={
+          <Welcome />
 
+        } />
+      </Routes>
 
-    </div >
+    </>
   );
 }
 
---
+++ src/components/landing/Welcome.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import About from './About'
+import ChooseUs from './ChooseUs'
+import Counter from './Counter'
+import Slider from './Slider'
+
+const Welcome = () => {
+    return (
+        <>
+            <Slider />
+            <Counter />
+            <About />
+            <ChooseUs />
+        </>
+
+    )
+}
+
+export default Welcome
