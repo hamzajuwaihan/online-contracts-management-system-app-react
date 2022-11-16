90d9d5b4fca956dd2f73aa42899db41de503208a
Hamza <108789747+hamzajuwaihan@users.noreply.github.com>
2022-11-16 12:25:43 +0300

    add user auth and routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-
 import { AuthProvider } from './components/auth';
-
 import AboutUs from "./components/about/AboutUs";
-
 import Container from './components/authentication/Container';
 import Login from './components/authentication/Login';
 import Profile from './components/authentication/Profile';
@@ -13,36 +10,43 @@ import { Address } from './components/landing/Address';
 import Footer from './components/landing/Footer';
 import NavBar from './components/landing/NavBar';
 import Welcome from './components/landing/Welcome';
+import { RequireAuth } from './RequireAuth';
 
 
 function App() {
   return (
     <>
       <AuthProvider>
-      <Address />
-      <NavBar />
-      <Routes>
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/" element={
-          <Welcome />
-        } />
-        <Route path='/login' element={
-          <Container>
-            <Login />
-          </Container>
-        } />
-        <Route path="/register" element={
-          <Container>
-            <Register />
-          </Container>
+        <Address />
+        <NavBar />
+        <Routes>
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/" element={
+            <Welcome />
+          } />
+          <Route path='/login' element={
+            <Container>
+              <Login />
+            </Container>
+          } />
+          <Route path="/register" element={
+            <Container>
+              <Register />
+            </Container>
 
-        } />
-        <Route path="*" element={
-          <h1>404 Not Found</h1>
-        } />
-      </Routes>
-      <Footer />
-</AuthProvider>
+          } />
+          <Route path='/profile' element={
+            <RequireAuth>
+              <Profile />
+            </RequireAuth>
+
+          } />
+          <Route path="*" element={
+            <h1>404 Not Found</h1>
+          } />
+        </Routes>
+        <Footer />
+      </AuthProvider>
     </>
   );
 }
