48f2790a8d4c2547c839d65f146d339cc74a6158
Ali-KL <khoolani.ali@gmail.com>
2022-11-16 09:48:47 +0300

    add about page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import { Address } from './components/landing/Address';
-import { NavBar } from './components/landing/NavBar';
-import Welcome from './components/landing/Welcome';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import AboutUs from "./components/about/AboutUs";
+import { Address } from "./components/landing/Address";
+import { NavBar } from "./components/landing/NavBar";
+import Welcome from "./components/landing/Welcome";
 
 function App() {
   return (
@@ -10,12 +11,9 @@ function App() {
       <Address />
       <NavBar />
       <Routes>
-        <Route path="/" element={
-          <Welcome />
-
-        } />
+        <Route path="/" element={<Welcome />} />
+        <Route path="/about" element={<AboutUs />} />
       </Routes>
-
     </>
   );
 }
--- src/components/landing/NavBar.js
+++ src/components/landing/NavBar.js
@@ -11,8 +11,8 @@ export const NavBar = () => {
             </button>
             <div className="collapse navbar-collapse" id="navbarCollapse">
                 <div className="navbar-nav ms-auto p-4 p-lg-0">
-                    <a href="index.html" className="nav-item nav-link active">Home</a>
-                    <a href="about.html" className="nav-item nav-link">About</a>
+                    <a href="/" className="nav-item nav-link active">Home</a>
+                    <a href="/about" className="nav-item nav-link">About</a>
                     <a href="service.html" className="nav-item nav-link">Service</a>
                     <a href="project.html" className="nav-item nav-link">Project</a>
                     <div className="nav-item dropdown">
