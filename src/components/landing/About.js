6f6465dcff9ca3e0b42258d74c585558569655d4
Hamza <108789747+hamzajuwaihan@users.noreply.github.com>
2022-11-19 18:01:43 +0300

    add images

--- src/components/landing/About.js
+++ src/components/landing/About.js
@@ -7,7 +7,7 @@ const About = () => {
                 <div className="row g-0 mx-lg-0">
                     <div className="col-lg-6 ps-lg-0 wow fadeIn" data-wow-delay="0.1s" style={{ minHeight: "400px" }}>
                         <div className="position-relative h-100">
-                            <img className="position-absolute img-fluid w-100 h-100" src="img/electronic-contract.jpg" style={{ objectFit: "cover"}} alt="" />
+                            <img className="position-absolute img-fluid w-100 h-100" src="img/electronic-contract.jpg" style={{ objectFit: "cover" }} alt="" />
                         </div>
                     </div>
                     <div className="col-lg-6 about-text py-5 wow fadeIn" data-wow-delay="0.5s">
@@ -15,8 +15,8 @@ const About = () => {
                             <h6 className="text-primary">About Us</h6>
                             <h1 className="mb-4">More than 25+ years in contract management</h1>
                             <p>We are a company interested in concluding contracts for major companies, and we are working to attract all beneficiaries by providing them with facilities</p>
-                           
-                            <a href="" className="btn btn-primary rounded-pill py-3 px-5 mt-3">Explore More</a>
+
+                            <a href="http://localhost:3000/about" className="btn btn-primary rounded-pill py-3 px-5 mt-3">Explore More</a>
                         </div>
                     </div>
                 </div>
--- src/components/landing/Employees.js
+++ src/components/landing/Employees.js
@@ -12,15 +12,15 @@ export const Employees = () => {
                     <div className="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                         <div className="team-item rounded overflow-hidden">
                             <div className="d-flex">
-                                <img className="img-fluid w-75" src="img/iages .jpg" alt="" />
+                                <img className="img-fluid w-75" src="img/garota7_475x600.jpeg" alt="" />
                                 <div className="team-social w-25">
-                                    <a className="btn btn-lg-square btn-outline-primary rounded-circle mt-3" href=""><i className="fab fa-facebook-f"></i></a>
-                                    <a className="btn btn-lg-square btn-outline-primary rounded-circle mt-3" href=""><i className="fab fa-twitter"></i></a>
-                                    <a className="btn btn-lg-square btn-outline-primary rounded-circle mt-3" href=""><i className="fab fa-instagram"></i></a>
+                                    <a className="btn btn-lg-square btn-outline-primary rounded-circle mt-3" href="https://ar-ar.facebook.com/"><i classNa00000000000000000000000me="fab fa-facebook-f"></i></a>
+                                    <a className="btn btn-lg-square btn-outline-primary rounded-circle mt-3" href="https://twitter.com/"><i className="fab fa-twitter"></i></a>
+                                    <a className="btn btn-lg-square btn-outline-primary rounded-circle mt-3" href="https://www.instagram.com/"><i className="fab fa-instagram"></i></a>
                                 </div>
                             </div>
                             <div className="p-4">
-                                <h5>Full Name</h5>
+                                <h5>Salma Naim</h5>
                                 <span>Designation</span>
                             </div>
                         </div>
@@ -28,15 +28,15 @@ export const Employees = () => {
                     <div className="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
                         <div className="team-item rounded overflow-hidden">
                             <div className="d-flex">
-                                <img className="img-fluid w-75" src="img/Elon_Musk_Royal_Society_(crop2).jpg" alt="" />
+                                <img className="img-fluid w-75" src="img/c1_475x600.jpg" alt="" />
                                 <div className="team-social w-25">
-                                    <a className="btn btn-lg-square btn-outline-primary rounded-circle mt-3" href=""><i className="fab fa-facebook-f"></i></a>
-                                    <a className="btn btn-lg-square btn-outline-primary rounded-circle mt-3" href=""><i className="fab fa-twitter"></i></a>
-                                    <a className="btn btn-lg-square btn-outline-primary rounded-circle mt-3" href=""><i className="fab fa-instagram"></i></a>
+                                    <a className="btn btn-lg-square btn-outline-primary rounded-circle mt-3" href="https://ar-ar.facebook.com/"><i className="fab fa-facebook-f"></i></a>
+                                    <a className="btn btn-lg-square btn-outline-primary rounded-circle mt-3" href="https://twitter.com/"><i className="fab fa-twitter"></i></a>
+                                    <a className="btn btn-lg-square btn-outline-primary rounded-circle mt-3" href="https://www.instagram.com/"><i className="fab fa-instagram"></i></a>
                                 </div>
                             </div>
                             <div className="p-4">
-                                <h5>Full Name</h5>
+                                <h5>Sara Ahmed</h5>
                                 <span>Designation</span>
                             </div>
                         </div>
@@ -44,15 +44,15 @@ export const Employees = () => {
                     <div className="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
                         <div className="team-item rounded overflow-hidden">
                             <div className="d-flex">
-                                <img className="img-fluid w-75" src="img/wnload.jpg" alt="" />
+                                <img className="img-fluid w-75" src="img/59as_475x600.jpg" alt="" />
                                 <div className="team-social w-25">
-                                    <a className="btn btn-lg-square btn-outline-primary rounded-circle mt-3" href=""><i className="fab fa-facebook-f"></i></a>
-                                    <a className="btn btn-lg-square btn-outline-primary rounded-circle mt-3" href=""><i className="fab fa-twitter"></i></a>
-                                    <a className="btn btn-lg-square btn-outline-primary rounded-circle mt-3" href=""><i className="fab fa-instagram"></i></a>
+                                    <a className="btn btn-lg-square btn-outline-primary rounded-circle mt-3" href="https://ar-ar.facebook.com/"><i className="fab fa-facebook-f"></i></a>
+                                    <a className="btn btn-lg-square btn-outline-primary rounded-circle mt-3" href="https://twitter.com/"><i className="fab fa-twitter"></i></a>
+                                    <a className="btn btn-lg-square btn-outline-primary rounded-circle mt-3" href="https://www.instagram.com/"><i className="fab fa-instagram"></i></a>
                                 </div>
                             </div>
                             <div className="p-4">
-                                <h5>Full Name</h5>
+                                <h5>Mariam Mohammed</h5>
                                 <span>Designation</span>
                             </div>
                         </div>
